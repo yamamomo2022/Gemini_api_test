5cd80edfbd8d02d80698ff1180e61fa4dd3b8e61
windows_inspiron14 <violetraisondetre@gmail.com>
2024-04-27 17:47:36 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Gemini_api_test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var chatSample = new MultiTurnChatSample();
+            await chatSample.GenerateContent();
+        }
+    }
+}
